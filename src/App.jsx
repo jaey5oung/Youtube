a7d2620add3806574546df8a8b22d1fa14b61b39
장재영 <jaey5oung@naver.com>
2021-05-23 18:08:15 +0900

    5/23 search setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
-import { React, useState, useEffect } from "react";
-import "./App.css";
-import VideoList from "./components/video_list/video_list";
+import { React, useState, useEffect } from 'react';
+import styles from './App.module.css';
+import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
 
 function App() {
-    const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState([]);
 
-    useEffect(() => {
-        const requestOptions = {
-            method: "GET",
-            redirect: "follow",
-        };
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
 
-        fetch(
-            "https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyDiBWcG3Luxfn6dOqM8RLKyP9n93xwnd7c&key=AIzaSyDiBWcG3Luxfn6dOqM8RLKyP9n93xwnd7c&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDiBWcG3Luxfn6dOqM8RLKyP9n93xwnd7c",
-            requestOptions
-        )
-            .then((response) => response.json())
-            .then((result) => setVideos(result.items))
-            .catch((error) => console.log("error", error));
-    }, []);
-    //마운트나업데이트가 되었을때 호출된다
-    //텅텅비어진거를 리턴하면 한번만 마운트가 됫을때만 호출된다
-    //[]안에 비디오를 넣으면 비디오가 업데이트될떄마다 콜백함수가 호출이된다
-    return <VideoList videos={videos} />;
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyDiBWcG3Luxfn6dOqM8RLKyP9n93xwnd7c&key=AIzaSyDiBWcG3Luxfn6dOqM8RLKyP9n93xwnd7c&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDiBWcG3Luxfn6dOqM8RLKyP9n93xwnd7c',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+  //마운트나업데이트가 되었을때 호출된다
+  //텅텅비어진거를 리턴하면 한번만 마운트가 됫을때만 호출된다
+  //[]안에 비디오를 넣으면 비디오가 업데이트될떄마다 콜백함수가 호출이된다
+  return (
+    <div className={styles.App}>
+      <SearchHeader />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,38 @@
+import { React, useRef } from 'react';
+import styles from './search_header.module.css';
+
+function SearchHeader({ onSearch }) {
+  const inputRef = useRef();
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    onSearch(value);
+  };
+  const onKeyPress = () => {
+    handleSearch();
+  };
+  const onClick = (event) => {
+    if (event === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img className={styles.img} src="./images/logo.png" alt="logo" />
+        <h1 className={styles.title}>Youtube</h1>
+      </div>
+      <input className={styles.input} type="search" placeholder="검색" onKeyPress={onKeyPress} />
+      <button className={styles.button} type="submit">
+        <img
+          className={styles.buttonImg}
+          src="./images/search.png"
+          alt="search"
+          onClick={onClick}
+        />
+      </button>
+    </header>
+  );
+}
+
+export default SearchHeader;

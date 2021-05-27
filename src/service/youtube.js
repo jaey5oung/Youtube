024fee47f135f0fdfd21448ee62e0d4bdc3fdffc
unknown <jaey5oung@naver.com>
2021-05-27 11:43:37 +0900

    05/27 반응형

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,33 +1,31 @@
 class Youtube {
-    constructor(key) {
-        this.key = key;
-        this.requestOptions = {
-            method: "GET",
-            redirect: "follow",
-        };
+    constructor(httpClient) {
+      this.youtube = httpClient;
     }
+  
     async mostPopular() {
-        return fetch(
-            `https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyDiBWcG3Luxfn6dOqM8RLKyP9n93xwnd7c&key=AIzaSyDiBWcG3Luxfn6dOqM8RLKyP9n93xwnd7c&part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-            this.requestOptions
-        )
-            .then((response) => response.json())
-            .then((result) => result.items)
-            .catch((error) => console.log("error", error));
+      const response = await this.youtube.get('videos', {
+        params: {
+          part: 'snippet',
+          chart: 'mostPopular',
+          maxResults: 25,
+        },
+      });
+      return response.data.items;
     }
-
+  
     async search(query) {
-        return fetch(
-            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
-            this.requestOptions
-        )
-            .then((response) => response.json())
-            .then((result) =>
-                result.items.map((item) => ({ ...item, id: item.id.videoId }))
-            )
-            .then((items) => items)
-            .catch((error) => console.log("error", error));
+      const response = await this.youtube.get('search', {
+        params: {
+          part: 'snippet',
+          maxResults: 25,
+          type: 'video',
+          q: query,
+        },
+      });
+      return response.data.items.map(item => ({ ...item, id: item.id.videoId }));
     }
-}
-
-export default Youtube;
+  }
+  
+  export default Youtube;
+  
